8767eb689c0dddb377c4c1c2b902eee3c9ee8a0f
RunzhongK-AI <rmei@k-ai.ai>
2025-02-14 17:35:37 +0100

    update chat css and fix doc url bug

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,15 +3,20 @@
         .message-history
             .message(v-for="message in messageHistory" :key="message.message" :style="{'justify-content': message.from === 'user' ? 'flex-end' : 'flex-start'}")
                 .assistance-message(v-if="message.from === 'assistance'")
-                    button.btn-icon.text-bold-14 BOT
+                    button.btn-icon.text-bold-14.name-image BOT
                     .message-text.assistance-color 
                         p.text-regular-14(v-html="message.message")
+                        p.text-regular-14(v-if="message.docList.length > 0") Source:
+                        p.text-regular-14(v-for="doc in message.docList" :key="doc.name" v-if="message.docList.length > 0")
+                            span(v-if="doc.url.includes('http')") 
+                                a.link(:href="doc.url" target="_blank") {{ doc.name }}
+                            span.link(@click="downloadFile(doc.name)" v-else) {{ doc.name }}
                 .user-message(v-if="message.from === 'user'")
                     .message-text.user-color 
                         p.text-regular-14 {{message.message}}
-                    button.btn-icon.text-bold-14 USER
+                    button.btn-icon.text-bold-14.name-image USER
             .assistance-message(v-if="loading")
-                button.btn-icon.text-bold-14 BOT
+                button.btn-icon.text-bold-14.name-image BOT
                 .message-text.assistance-color 
                     p.text-regular-14 Searching... {{ loadingPercentage }}%
         .input-container(v-if="step === 'conversation'")
@@ -23,12 +28,13 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { KaiStudio } from 'kaistudio-sdk-js';
+import Buffer from 'vue-buffer';
 
 // if you are using Saas
 const kaiSearch = new KaiStudio({ organizationId: import.meta.env.VITE_ORGANIZATION_ID, instanceId: import.meta.env.VITE_INSTANCE_ID, apiKey: import.meta.env.VITE_API_KEY });
 const multiDocuments: boolean = import.meta.env.VITE_MULTI_DOCUMENTS === 'true';
 
-const messageHistory = ref<{ from: string; message: string }[]>([]);
+const messageHistory = ref<{ from: string; message: string; docList: string[]; }[]>([]);
 const userMessage = ref('');
 const userMessageTemp = ref('');
 const conversationId = ref('');
@@ -36,10 +42,11 @@ const step = ref('conversation');
 const loadingPercentage = ref(0);
 const loading = ref(false);
 let loadingInterval: number | null = null;
+const documentsList = ref<{ name: string; url: string }[]>([]);
 
 const sendMessage = async () => {
     try {
-        const userMsg = { from: 'user', message: userMessage.value };
+        const userMsg = { from: 'user', message: userMessage.value, docList: [] };
         messageHistory.value.push(userMsg);
         userMessageTemp.value = userMessage.value;
         userMessage.value = '';
@@ -98,25 +105,20 @@ function stopLoading() {
 async function processSearchResult(result: any) {
     try {
         if (result['action'] == 'SEARCH' && result['datas']['isAnswered']) {
-            messageHistory.value.push({ from: 'assistance', message: 'Answer:' });
-            messageHistory.value.push({ from: 'assistance', message: result['content'] });
-
+            messageHistory.value.push({ from: 'assistance', message: "Answer:", docList: [] });
+            let docList = [];
             if (result['datas']['documents'].length > 0) {
-                let documents = '';
-                for (let doc of result['datas']['documents']) {
-                    documents += `name: ${doc.name}<br>url: <a href="${doc.url}" style="color: var(--primary-color)" target="_blank">${doc.url}</a><br><br>`;
-                }
-                messageHistory.value.push({ from: 'assistance', message: 'Source:' });
-                messageHistory.value.push({ from: 'assistance', message: documents });
+                docList = result['datas']['documents']
             }
+            messageHistory.value.push({ from: 'assistance', message: result['content'], docList: docList });
         } else {
-            messageHistory.value.push({ from: 'assistance', message: result['content'] });
-            if ('reason' in result['datas'] && result['datas']['reason'] != '') {
-                messageHistory.value.push({ from: 'assistance', message: result['datas']['reason'] });
+            messageHistory.value.push({ from: 'assistance', message: result['content'], docList: []});
+            if ('datas' in result && 'reason' in result['datas'] && result['datas']['reason'] != '') {
+                messageHistory.value.push({ from: 'assistance', message: result['datas']['reason'], docList: [] });
             }
-            step.value = 'conversation';
+            step.value = 'conversation'
         }
-    } catch (error) {
+    } catch(error) {
         console.log(error);
     }
 }
@@ -127,85 +129,118 @@ function init() {
     step.value = 'conversation';
 }
 
+async function downloadFile(fileName: string) {
+    let file = await kaiSearch.fileInstance().downloadFile(fileName)
+    if (file) {
+        const buffer = Buffer.from(file);
+        const blob = new Blob([buffer]);
+        const url = window.URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        document.body.appendChild(a);
+        a.setAttribute('style', 'display: none');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+}
+
 onMounted(() => {
-    messageHistory.value.push({ from: 'assistance', message: 'Hello, how can I help you today?' });
+    messageHistory.value.push({ from: 'assistance', message: 'Hello, how can I help you today?', docList: []  });
 });
 </script>
 
+    
 <style lang="scss" scoped>
-.search-chat {
-    width: 799px;
-    height: 100%;
-    background-color: var(--dark-grey-color);
-    border: 1px solid var(--color-border);
-    border-radius: 12px;
-    padding: 11px 11px 30px 11px;
-    display: flex;
-    flex-direction: column;
-    overflow: scroll;
-
-    .message-history {
-        width: 100%;
-        height: 100%;
-        border-radius: 10px;
-        background-color: var(--grey-4-color);
-        padding: 20px;
+    .search-chat {
+        width: 900px;
+        height: 500px;
+        background-color: var(--dark-grey-color);
+        border: 1px solid var(--color-border);
+        border-radius: 12px;
+        padding: 11px 11px 30px 11px;
+        display: flex;
+        flex-direction: column;
         overflow: scroll;
-        button {
-            height: 50px;
-            width: 50px;
-            justify-content: center;
-        }
-
-        .assistance-message {
-            display: flex;
-            flex-direction: row;
-            width: fit-content;
-            max-width: 80%;
-            align-items: center;
-            .assistance-color {
-                background-color: var(--grey-5-color);
-            }
-        }
-
-        .message-text {
+    
+        .message-history {
             width: 100%;
-            margin: 10px 20px;
-            color: white;
-            padding: 10px 10px 10px 10px;
+            height: 100%;
             border-radius: 10px;
-            word-break: break-word;
-            white-space: pre-wrap;
-        }
+            background-color: var(--grey-5-color);
+            padding: 20px;
+            overflow: scroll;
+            .name-image {
+                height: 50px;
+                width: 50px;
+                justify-content: center;
+            }
 
-        .message {
-            display: flex;
-            flex-direction: row;
-            width: 100%;
-
-            .user-message {
+            .assistance-message {
                 display: flex;
                 flex-direction: row;
                 width: fit-content;
                 max-width: 80%;
                 align-items: center;
-                .user-color {
+                .assistance-color {
                     background-color: var(--color-border);
                 }
+
+                .link {
+                    color: var(--primary-color);
+                    cursor: pointer;
+                }
+            }
+
+            .message-text {
+                width: 100%;
+                margin: 10px 20px;
+                color: white;
+                padding: 10px 10px 10px 10px;
+                border-radius: 10px;
+                word-break: break-word;
+                white-space: pre-wrap;
+                line-height: 1.5;
+                .button-download {
+                    margin-left: 20px;
+                }
+            }
+    
+            .message {
+                display: flex;
+                flex-direction: row;
+                width: 100%;     
+                
+                
+    
+                .user-message {
+                    display: flex;
+                    flex-direction: row;
+                    width: fit-content;
+                    max-width: 80%;
+                    align-items: center;
+                    .user-color {
+                        background-color: var(--color-border);
+                    }
+                }
+    
+                
+            }
+        }
+        .input-container {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            margin-top: 20px;
+    
+            input {
+                background-color: white;
+                color: black;
+                max-width: none;
+                flex-grow: 1;
+                margin-right: 20px;
             }
         }
     }
-    .input-container {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-        margin-top: 20px;
-
-        input {
-            background-color: white;
-            color: black;
-        }
-    }
-}
 </style>
+    
